190ffdf6119f199fd85ce76fed82d0e1443ebe92
Aaquib Faridi <aaquib.faridi@tcs.com>
2021-05-02 15:01:08 +0530

    initial commit

---
+++ precache-manifest.20aab59c7a2ef451ff34935c55b4b0e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb3b00c28fa3db4d7a1285130bcc537",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8948f3e0a4b688f13b16",
+    "url": "/static/css/main.6e648083.chunk.css"
+  },
+  {
+    "revision": "74b9e2981a9ed0f034de",
+    "url": "/static/js/2.e9fabf8a.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.e9fabf8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8948f3e0a4b688f13b16",
+    "url": "/static/js/main.1c3bf860.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
